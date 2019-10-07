facb35f372e5fcc9195123724a9a0b948360589b
Anne Terechowa <annevonwolffen@gmail.com>
2019-10-07 04:55:57 +0300

    add convert_activity layout

--- app/src/main/java/com/example/simpleconverter/ConverterActivity.java
+++ app/src/main/java/com/example/simpleconverter/ConverterActivity.java
@@ -1,4 +1,16 @@
 package com.example.simpleconverter;
 
-public class ConverterActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConverterActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convert);
+
+
+
+    }
 }
